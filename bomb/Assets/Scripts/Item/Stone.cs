e863aa1377169c18af85472b8324825f112dd203
FrozenThera <dongmin11004@korea.ac.kr>
2022-05-17 04:20:31 +0900

    .

--- bomb/Assets/Scripts/Item/Stone.cs
+++ bomb/Assets/Scripts/Item/Stone.cs
@@ -12,7 +12,7 @@ public class Stone : Item
 
     [Command]
     public void CmdStoneSpawn(){
-        GameObject projectile = Instantiate(player.curItemObj, player.transform.position + 5 * (player.coll.bounds.size.x + 0.1f) * (player.isHeadingRight ? Vector3.right : Vector3.left), Quaternion.identity);
+        GameObject projectile = Instantiate(player.curItemObj, player.transform.position + (player.coll.bounds.size.x + 0.1f) * (player.isHeadingRight ? Vector3.right : Vector3.left), Quaternion.identity);
         projectile.GetComponent<StoneProjectile>().dir = player.isHeadingRight;
         NetworkServer.Spawn(projectile);
     }
